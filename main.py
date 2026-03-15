0790041c75de17e66a64bb3c4e20a466a6043740
AidenYue <2687927133@qq.com>
2026-03-15 15:15:58 +0800

    引入JSONOutputParser

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
 
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+
+class Work(BaseModel):
+    title: str = Field(..., description="Title of the work")
+    description: str = Field(..., description="Description of the work")
+
 
 os.environ["OPENAI_API_BASE"] = "https://api.deepseek.com/v1"
-prompt_template = ChatPromptTemplate.from_messages([
-    ("system", "Translate the following from English into Chinese:"),
-    ("user", "{text}")
-])
+parser = JsonOutputParser(pydantic_object=Work)
+prompt_template = PromptTemplate(
+    template="列举3部{author}的作品。\n{format_instructions}",
+    input_variables=["author"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
 
 model = ChatOpenAI(model="deepseek-chat")
-parser = StrOutputParser()
 chain = prompt_template | model | parser
-result = chain.invoke({"text": "Welcome to LLM application development!"})
+result = chain.invoke({"author": "刘震云"})
 print(result)
