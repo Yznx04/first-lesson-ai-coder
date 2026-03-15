5d1781f190eb9c98002dd710088add6d28d600ad
AidenYue <2687927133@qq.com>
2026-03-15 14:25:18 +0800

    first code by openai

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+
+os.environ["OPENAI_API_BASE"] = "https://api.deepseek.com/v1"
+messages = [
+    SystemMessage(content="Translate the following from English into Chinese"),
+    HumanMessage(content="Welcome to LLM application development")
+]
+
+model = ChatOpenAI(model="deepseek-chat")
+result = model.invoke(messages)
+print(result)
